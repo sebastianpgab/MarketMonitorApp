d7c98e5c4abeb50e64a5bba90a938be38d27efa9
sebastianpgab <sebastianpgab@gmail.com>
2024-06-03 23:01:19 +0200

    Add UpdatePageNumberInLink method to KolbaStrategy class

--- Services/ProductPatterns/KolbaStrategy.cs
+++ Services/ProductPatterns/KolbaStrategy.cs
@@ -1,7 +1,10 @@
 ﻿using HtmlAgilityPack;
 using MarketMonitorApp.Entities;
 using MarketMonitorApp.Services.ProductsStrategy;
+using System;
+using System.Globalization;
 using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
 
 namespace MarketMonitorApp.Services.ProductPatterns
 {
@@ -12,12 +15,56 @@ namespace MarketMonitorApp.Services.ProductPatterns
             var buttonText = FindButtonWithText(web, baseUrl);
             var maxNumber = FindMaxNumberInButtonText(buttonText);
 
-            return 1;
+            return 4;
         }
-
         public IEnumerable<Product> GetProducts(string baseUrl, int currentPage)
         {
-            throw new NotImplementedException();
+            var web = new HtmlWeb();
+            var pageUrl = UpdatePageNumberInLink(baseUrl, currentPage);
+            var document = web.Load(pageUrl);
+
+            var products = new List<Product>();
+            var productNodes = document.DocumentNode.QuerySelectorAll(".product__name");
+            int i = 0;
+            foreach (var productNode in productNodes)
+            {
+
+                var newProduct = new Product();
+                newProduct.IdProduct = i++.ToString();
+                newProduct.Name = "";
+                newProduct.Price = 20;
+
+                products.Add(newProduct);
+
+            }
+
+            return products;
+        }
+
+        private string UpdatePageNumberInLink(string url, int currentPage)
+        {
+            int queryStartIndex = url.IndexOf('?');
+            if (queryStartIndex == -1)
+            {
+                return url;
+            }
+
+            string baseUrl = url.Substring(0, queryStartIndex);
+            string query = url.Substring(queryStartIndex + 1);
+            string[] parameters = query.Split('&');
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                if (parameters[i].StartsWith("page="))
+                {
+                    parameters[i] = "page=" + currentPage;
+                    break;
+                }
+            }
+
+            string newQuery = string.Join("&", parameters);
+
+            return baseUrl + "?" + newQuery;
         }
 
         public string FindButtonWithText(HtmlWeb web, string baseUrl)

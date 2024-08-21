60f7e60b948de70eecdcb92b100879cf04cf8223
sebastianpgab <sebastianpgab@gmail.com>
2024-08-21 23:30:20 +0200

    Add exception when name of product is null or empty.

--- MarketMonitor/Services/ProductPatterns/KolbaStrategy.cs
+++ MarketMonitor/Services/ProductPatterns/KolbaStrategy.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using MarketMonitorApp.Entities;
 using MarketMonitorApp.Services.ProductsStrategy;
+using Microsoft.AspNetCore.Server.IIS.Core;
 using System;
 using System.Globalization;
 using System.Reflection.Metadata;
@@ -56,6 +57,10 @@ namespace MarketMonitorApp.Services.ProductPatterns
                 var linkHtml = productNode.ProdName.QuerySelector("a").GetAttributeValue("href", string.Empty);
                 var productId = GetProductId(linkHtml);
                 var productNameNode = productNode.ProdName.InnerText.Trim();
+                if(productNameNode == null || productNameNode == string.Empty)
+                {
+                    throw new NullReferenceException("Product name node is null or empty.");
+                }
                 var priceElement = productNode.ProdPrice.QuerySelector(".flex .flex .text-xl");
 
                 var price = priceElement != null ? priceElement.InnerText.Trim() : "0";

b0a9e46746557f597f64c45131da4c5b3d3b4211
sebastianpgab <sebastianpgab@gmail.com>
2024-08-21 23:32:24 +0200

    improve and add unit tests to KolbaStrategyTests + add new libary FluentAssertions

--- MarketMonitorTests/KolbaStrategyTests.cs
+++ MarketMonitorTests/KolbaStrategyTests.cs
@@ -36,7 +36,7 @@ namespace MarketMonitorTests
                  <html>
                    <body>
                      <div class='product__name'> 
-                        <a href='/product/53101,kamera-termowizyjna-termowizor-hikmicro-by-hikvision-falcon-fq50'>Puszka Real Hunter 300ml</a>
+                        <a href='/product/53101,kamera-termowizyjna-termowizor-hikmicro-by-hikvision-falcon-fq50'>termowizor hikmicro by hikvision falcon fq50</a>
                      </div>
                      <div class='product__price'>
                          <div class='flex'>
@@ -50,6 +50,48 @@ namespace MarketMonitorTests
                 ",
                 "https://kolba.pl/pl/search?page=1&sort=default&query=hikmicro", 
                 5
+            };
+            yield return new object[]
+            {
+                @"
+                 <html>
+                   <body>
+                     <div class='product__name'> 
+                        <a href='/product/53102,puszka-real-hunter-300ml'>Puszka Real Hunter 300ml</a>
+                     </div>
+                     <div class='product__price'>
+                         <div class='flex'>
+                             <div class='flex'>
+                                 <div class='text-xl'></div>
+                             </div>
+                         </div>
+                     </div>
+                   </body>
+                 </html>
+                ",
+                "https://kolba.pl/pl/search?page=1&sort=default&query=hikmicro",
+                9
+            };
+            yield return new object[]
+            {
+                @"
+                 <html>
+                   <body>
+                     <div class='product__name'> 
+                        <a href='/product/123,kamera-termowizyjna-termowizor-hikmicro-by-hikvision-falcon-fq50'>Kamera termowizyjna Falcon Fq50</a>
+                     </div>
+                     <div class='product__price'>
+                         <div class='flex'>
+                             <div class='flex'>
+                                 <div class='text-xl'></div>
+                             </div>
+                         </div>
+                     </div>
+                   </body>
+                 </html>
+                ",
+                "https://kolba.pl/pl/search?page=1&sort=default&query=hikmicro",
+                3
             };
         }
 
@@ -92,10 +134,77 @@ namespace MarketMonitorTests
                            .Callback<string>(capturedUrl => expectedUrl = capturedUrl)
                            .Returns(_htmlDocument);
 
-            _kolbaStrategy.GetProducts(url, 5);
+            _kolbaStrategy.GetProducts(url, currentPage);
 
-            Assert.Equal("https://kolba.pl/pl/search?page=5&sort=default&query=hikmicro", expectedUrl);
+            Assert.Equal("https://kolba.pl/pl/search?page="+currentPage+"&sort=default&query=hikmicro", expectedUrl);
         }
+
+        [Theory]
+        [MemberData(nameof(ProductSearchTestData))]
+        public void GetProducts_ShouldReturnCorrectProducts(string html, string url, int currentPage)
+        {
+            _htmlDocument.LoadHtml(html);
+            _htmlWebAdapter.Setup(p => p.Load(It.IsAny<string>())).Returns(_htmlDocument);
+
+            //Act
+            var result = _kolbaStrategy.GetProducts(url, currentPage).ToList();
+
+            // Assert
+            if (currentPage == 1)
+            {
+                Assert.Single(result);
+                Assert.Equal("53101", result[0].IdProduct);
+                Assert.Equal("termowizor hikmicro by hikvision falcon fq50", result[0].Name);
+                Assert.Equal(23.00m, result[0].Price);
+            }
+            else if (currentPage == 9)
+            {
+                Assert.Single(result);
+                Assert.Equal("53102", result[0].IdProduct);
+                Assert.Equal("Puszka Real Hunter 300ml", result[0].Name);
+                Assert.Equal(0m, result[0].Price);
+            }
+            else if (currentPage == 3)
+            {
+                Assert.Single(result);
+                Assert.Equal("123", result[0].IdProduct);
+                Assert.Equal("Kamera termowizyjna Falcon Fq50", result[0].Name);
+                Assert.Equal(0m, result[0].Price);
+            }
+        }
+
+
+        [Fact]
+        public void GetProducts_MissingProductNameSelector_ShouldThrowNullReferenceException()
+        {
+            string html =
+            @"
+                <html>
+                <body>
+                    <div class='product__name'> 
+                    <a href='/product/123,kamera-termowizyjna-termowizor-hikmicro-by-hikvision-falcon-fq50'></a>
+                    </div>
+                    <div class='product__price'>
+                        <div class='flex'>
+                            <div class='flex'>
+                                <div class='text-xl'></div>
+                            </div>
+                        </div>
+                    </div>
+                </body>
+                </html>
+            ";
+
+            _htmlDocument.LoadHtml(html);
+            _htmlWebAdapter.Setup(p => p.Load(It.IsAny<string>())).Returns(_htmlDocument);
+
+            //Act
+            Action action = () => { _kolbaStrategy.GetProducts("url", 1); };
+
+            //Assert
+            Assert.Throws<NullReferenceException>(action);
+        }
+
 
     }
 }
